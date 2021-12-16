768fa4da38b7660d65970c927b3863cb59f2e6a1
MrMultiRel <71727874+MrMultiRel@users.noreply.github.com>
2021-12-16 21:42:00 +0300

    Add files via upload

---
+++ TranslatorTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TranslatorTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TranslatorTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b4b9ff78-537d-432c-8867-1e8d0aa54902")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TranslatorTests/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Project;
+
+namespace TranslatorTests
+{
+    [TestClass]
+    public class Translator
+    {
+        [TestMethod]
+        public void Translate_15_Десятичной_Двоичную_Returned1111()
+        {
+            Number number = new Number("15", "Десятичной");
+
+            string arg1 = "Двоичную";
+            string expected = "1111";
+
+            string result = number.Translate(arg1);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Некорректный ввод")]
+        public void Translate_12F_Восьмеричной_Десятичную_ArgumentException()
+        {
+            Number number = new Number("12F", "Восьмеричной");
+
+            string arg1 = "Десятичную";
+
+            string result = number.Translate(arg1);
+        }
+    }
+}
